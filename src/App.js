23d1c124760b9f6d3545660eddc2d69011ae0c67
Inzamam Ahmad <enzee274@gmail.com>
2022-06-04 22:13:50 -0700

    Added Navbar inside our App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import Home from "./routes/home/home.component";
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Outlet} from "react-router-dom"
+import Navigation from "./routes/navigation/navigation.component";
+
+
+const Shop = ()=>{
+  return <div>
+    <h1>Hello Shopping</h1>
+  </div>
+}
 
 
 const  App= ()=> {
- return( <Routes >
-          <Route path="/" element={<Home />}  />
-          
-        </Routes>
+ return( 
+          <Routes >
+            <Route path="/" element={<Navigation />}>
+            <Route index element={<Home />} />
+            <Route path="shop" element={<Shop />} />
+
+            </Route>
+            
+          </Routes>
         )
 }
 
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,5 +1,5 @@
 import Directory from "../../components/directory/directory.component";
-
+import { Outlet } from "react-router-dom";
 
 const  Home = ()=> {
   const categories = [
@@ -31,7 +31,12 @@ const  Home = ()=> {
   ]
   
   return (
-    <Directory categories={categories}/>
+    <div>
+      <Outlet />
+      <Directory categories={categories}/>
+      
+    </div>
+    
   );
 }
 
