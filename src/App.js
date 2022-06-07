71618cb1a46cf841309d98db0a347a8251856d3f
Inzamam Ahmad <enzee274@gmail.com>
2022-06-07 09:13:05 -0700

    Added Route inside our website of Sign IN Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./routes/home/home.component";
 import {Routes, Route, Outlet} from "react-router-dom"
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 
 const Shop = ()=>{
@@ -16,6 +17,7 @@ const  App= ()=> {
             <Route path="/" element={<Navigation />}>
             <Route index element={<Home />} />
             <Route path="shop" element={<Shop />} />
+            <Route path="sign-in" element={<SignIn />} />
 
             </Route>
             
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,14 @@
+
+
+const SignIn = ()=>{
+    return (
+        <div>
+            <h1>
+                Sign In Page
+            </h1>
+        </div>
+    )
+}
+
+
+export default SignIn
