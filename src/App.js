a7ad5645bade733be0de1affe1cc8ecfc99c6b9a
Inzamam Ahmad <enzee274@gmail.com>
2022-06-15 10:23:39 -0700

    Added fake json data for shop content

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import Home from "./routes/home/home.component";
 import {Routes, Route} from "react-router-dom"
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 
-
-const Shop = ()=>{
-  return <div>
-    <h1>Hello Shopping</h1>
-  </div>
-}
 
 
 const  App= ()=> {
